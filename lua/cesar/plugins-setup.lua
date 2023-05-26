15d8294bc5a7d3a0673831915a30baeb1cb37a11
Cesar Rebolledo <104473118+cesrebol@users.noreply.github.com>
2023-05-25 23:05:21 -0400

    autocomplete  (#7)
    * Telescope with fzf done. ripgrep was added.
    
    * saving - autocomplete section almost done

--- lua/cesar/plugins-setup.lua
+++ lua/cesar/plugins-setup.lua
@@ -51,6 +51,19 @@ return packer.startup(function(use)
   use({"nvim-telescope/telescope-fzf-native.nvim", run = "make" })
   use({"nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
+
+  -- autocompletion 
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+
+
+  -- snippets 
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+  use("rafamadriz/friendly-snippets") 
+
+
   if packer_bootstrap then
     require("packer").sync()
   end

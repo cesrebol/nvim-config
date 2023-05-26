af2fa96b29fbee1d24332d00dca4932fe6fb1508
cesar <rebolledoces@gmail.com>
2023-05-25 22:08:14 -0400

    Telescope with fzf done. ripgrep was added.

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,6 +109,16 @@ _G.packer_plugins = {
     path = "/home/cesar/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/home/cesar/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/cesar/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
   ["vim-maximizer"] = {
     loaded = true,
     path = "/home/cesar/.local/share/nvim/site/pack/packer/start/vim-maximizer",
